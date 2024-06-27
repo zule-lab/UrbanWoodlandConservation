aecede7bab0816bea5f6652f5248f1725d68b0fb
icrichmond <bellarichmond@proton.me>
2024-06-27 14:46:18 -0400

    missing selection for forested parks

--- R/plot_data.R
+++ R/plot_data.R
@@ -1,12 +1,14 @@
 plot_data <- function(parks, diversity, canopy, invasive){
   
+  parks_for <- parks[grepl("FOR", parks[["PlotID"]]), ]
+  
   parks_sel_plot <- parks_for %>%
     select(c(Park, PlotID, Park.size, Park.age, Established, Conservation.area, VegetationLevels)) %>%
     distinct() %>% 
     mutate(Complexity = stri_count_words(VegetationLevels))
   
-  cdp <- full_join(canopy_plots, diversity_plots, by = join_by("Park" == "PlotID"))
-  cdip <- full_join(cd, invasive_plots, by = join_by("Park" == "PlotID"))
+  cdp <- full_join(canopy, diversity, by = join_by("Park" == "PlotID"))
+  cdip <- full_join(cdp, invasive, by = join_by("Park" == "PlotID"))
   full_plots <- full_join(cdip, parks_sel_plot, by = join_by("Park" == "PlotID"))
   
   return(full_plots)
