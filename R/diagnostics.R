acfd70284039414ab873de7cd9bb55e47fa78c65
icrichmond <bellarichmond@proton.me>
2024-07-15 15:38:20 -0400

    add canopy cover model

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -1,4 +1,4 @@
-diagnostics <- function(sr_park, shan_park, complexity_park, inv_sp_park, inv_stems_park){
+diagnostics <- function(sr_park, shan_park, complexity_park, can_park, inv_sp_park, inv_stems_park){
 
   
   pdf('figures/diagnostics/model_diagnostics.pdf')
@@ -6,6 +6,7 @@ diagnostics <- function(sr_park, shan_park, complexity_park, inv_sp_park, inv_st
   print(resid_plots(sr_park, "Species Richness"))
   print(resid_plots(shan_park, "Shannon Diversity"))
   print(resid_plots(complexity_park, "Vegetative Complexity"))
+  print(resid_plots(can_park, "Canopy Cover"))
   print(resid_plots(inv_sp_park, "Invasive Species"))
   print(resid_plots(inv_stems_park, "Invasive Stems"))
   
